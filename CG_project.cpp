ba6e448f153684a00385d5081c7c3ab12ab74a33
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-08 17:09:31 +0900

    롤러코스터 정점에 원기둥 생성

--- CG_project.cpp
+++ CG_project.cpp
@@ -87,6 +87,10 @@ float len_x[5], len_z[5];  // 기구 가로 세로 길이
 float rides_radian[5];     // 기구 회전
 float tree_x[30], tree_z[30];   // 나무 좌표
 int tree_cnt;     // 설치된 나무의 개수 
+BOOL point_sel[8];  // 롤러코스터 정점 선택
+int point_sel_index;   // 선택된 정점의 인덱스
+float point_save[9][3];  // 초기 롤러코스터 위치;
+BOOL key_c, key_t;  // 롤러코스터 편집
 
 void menu() {
 	printf("------------ top view 명령어-------------\n");
@@ -103,6 +107,8 @@ void menu() {
 	printf("4 : 회전목마 시점\n");
 	printf("w, a, s, d : 카메라 이동\n");
 	printf("------------- 공용 명령어 ---------------\n");
+	printf("c : 롤러코스터 정점 위치 변경\n");
+	printf("t : c 상태에서 시점 변경\n");
 	printf("v : 시점 변경\n");
 	printf("` : 리셋\n");
 	printf("q : 프로그램 종료\n");
@@ -120,6 +126,7 @@ void reset() {
 	view_check = 1;
 	rides_sel_cnt = 0;
 	tree_cnt = 30;
+	key_c = key_t = false;
 	// 기구의 길이
 	len_x[1] = 9, len_x[2] = 5, len_x[3] = len_x[4] = 4;
 	len_z[1] = 3, len_z[2] = 2, len_z[3] = len_z[4] = 4;
@@ -133,11 +140,15 @@ void reset() {
 	}
 	// 롤러코스터 정점 랜덤값
 	for (int i = 0; i < 8; ++i) {
+		point[i][0] = point_save[i][0];
 		point[i][1] = (rand() % 501 + 500) * 0.01;    // 5 <= y <= 10
+		point[i][2] = point_save[i][2];
+		point_sel[i] = false;
 	}
 	point[8][0] = point[0][0];
 	point[8][1] = point[0][1];
 	point[8][2] = point[0][2];
+	point_sel_index = 0;
 	// 나무 위치
 	for (int i = 0; i < tree_cnt; ++i) {
 		tree_x[i] = (rand() % 2801 - 1400) * 0.01;
@@ -179,6 +190,10 @@ void main(int argc, char** argv)
 	glutCreateWindow("Amusement Park");
 	glewExperimental = GL_TRUE;
 	menu();
+	for (int i = 0; i < 9; ++i) {
+		point_save[i][0] = point[i][0];
+		point_save[i][2] = point[i][2];
+	}
 	reset();
 	glewInit();
 	make_shaderProgram();
@@ -210,156 +225,189 @@ GLvoid drawScene() {
 	unsigned int viewPosLocation = glGetUniformLocation(shaderProgramID, "viewPos");
 	glUniform3f(viewPosLocation, cameraPos_x, cameraPos_y, cameraPos_z);
 
-	if (view_check == 0) {
-		glm::vec3 cameraPos = glm::vec3(0.0f, 5.0f, 0.0f);      //--- 카메라 위치
-		glm::vec3 cameraDirection = glm::vec3(0.0f, 0.0f, 0.0f); //--- 카메라 바라보는 방향
-		glm::vec3 cameraUp = glm::vec3(0.0f, 0.0f, 1.0f);        //--- 카메라 위쪽 방향
+	if (!key_c) {
+		// top view 
+		if (view_check == 0) {
+			glm::vec3 cameraPos = glm::vec3(0.0f, 5.0f, 0.0f);
+			glm::vec3 cameraDirection = glm::vec3(0.0f, 0.0f, 0.0f);
+			glm::vec3 cameraUp = glm::vec3(0.0f, 0.0f, 1.0f);
 
-		glm::mat4 vTransform = glm::mat4(1.0f);
-		vTransform = glm::lookAt(cameraPos, cameraDirection, cameraUp);
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &vTransform[0][0]);
+			glm::mat4 vTransform = glm::mat4(1.0f);
+			vTransform = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &vTransform[0][0]);
 
-		glm::mat4 pTransform = glm::mat4(1.0f);
-		pTransform = glm::ortho(16.0f, -16.0f, 16.0f, -16.0f, -16.0f, 16.0f);
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, &pTransform[0][0]);
-	}
-	else if (view_check == 1) {
-		cameraDirection_x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
-		cameraDirection_y = sin(glm::radians(pitch));
-		cameraDirection_z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+			glm::mat4 pTransform = glm::mat4(1.0f);
+			pTransform = glm::ortho(16.0f, -16.0f, 16.0f, -16.0f, -16.0f, 16.0f);
+			glUniformMatrix4fv(projLoc, 1, GL_FALSE, &pTransform[0][0]);
+		}
+		// front view
+		else if (view_check == 1) {
+			cameraDirection_x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+			cameraDirection_y = sin(glm::radians(pitch));
+			cameraDirection_z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
 
-		cameraPos = glm::vec3(cameraPos_x, cameraPos_y, cameraPos_z);      //--- 카메라 위치
-		cameraDirection = glm::vec3(cameraDirection_x, cameraDirection_y, cameraDirection_z); //--- 카메라 바라보는 방향
-		cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);        //--- 카메라 위쪽 방향
+			cameraPos = glm::vec3(cameraPos_x, cameraPos_y, cameraPos_z);
+			cameraDirection = glm::vec3(cameraDirection_x, cameraDirection_y, cameraDirection_z);
+			cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
-		glm::mat4 vTransform = glm::mat4(1.0f);
-		vTransform = glm::lookAt(cameraPos, cameraPos + cameraDirection, cameraUp);
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &vTransform[0][0]);
+			glm::mat4 vTransform = glm::mat4(1.0f);
+			vTransform = glm::lookAt(cameraPos, cameraPos + cameraDirection, cameraUp);
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &vTransform[0][0]);
 
-		glm::mat4 pTransform = glm::mat4(1.0f);
-		pTransform = glm::perspective(glm::radians(fov), (float)window_w / (float)window_h, 0.1f, 200.0f);
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, &pTransform[0][0]);
-	}
+			glm::mat4 pTransform = glm::mat4(1.0f);
+			pTransform = glm::perspective(glm::radians(fov), (float)window_w / (float)window_h, 0.1f, 200.0f);
+			glUniformMatrix4fv(projLoc, 1, GL_FALSE, &pTransform[0][0]);
+		}
+		// 롤러코스터
+		else if (view_check == 2) {}
+		// 관람차
+		else if (view_check == 3) {}
+		// 자이로드롭
+		else if (view_check == 4) {}
+		// 회전 목마
+		else if (view_check == 5) {}
 
-	// 놀이 기구
-	for (int i = 1; i < 5; ++i) {
-		if (rides_sel_check[i] == true || rides_install_check[i] == true) {
-			glm::mat4 border = glm::mat4(1.0f);
-			glm::mat4 border_T = glm::mat4(1.0f);
-			glm::mat4 border_R = glm::mat4(1.0f);
-			border_T = glm::translate(border_T, glm::vec3(rides_x[i], 0.0, rides_z[i]));
-			border_R = glm::rotate(border_R, glm::radians(rides_radian[i]), glm::vec3(0.0, 1.0, 0.0));
-			border = border_T * border_R;
-			if (view_check == 0) {
-				glLineWidth(2);
-				if (!rides_collision[i])
-					glUniform3f(objColorLocation, 0.0, 1.0, 0.0);
-				else
+		// 놀이 기구
+		for (int i = 1; i < 5; ++i) {
+			if (rides_sel_check[i] == true || rides_install_check[i] == true) {
+				glm::mat4 border = glm::mat4(1.0f);
+				glm::mat4 border_T = glm::mat4(1.0f);
+				glm::mat4 border_R = glm::mat4(1.0f);
+				border_T = glm::translate(border_T, glm::vec3(rides_x[i], 0.0, rides_z[i]));
+				border_R = glm::rotate(border_R, glm::radians(rides_radian[i]), glm::vec3(0.0, 1.0, 0.0));
+				border = border_T * border_R;
+				if (view_check == 0) {
+					glLineWidth(2);
+					if (!rides_collision[i])
+						glUniform3f(objColorLocation, 0.0, 1.0, 0.0);
+					else
+						glUniform3f(objColorLocation, 1.0, 0.0, 0.0);
+					glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(border));
+					glBindVertexArray(vao[i + 10]);
+					glDrawArrays(GL_LINE_LOOP, 0, 4);
+				}
+				if (i == 1) { // 롤러코스터인 경우
+					// 정점 찍기
+					glPointSize(10.0f);
+					glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(border));
 					glUniform3f(objColorLocation, 1.0, 0.0, 0.0);
-				glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(border));
-				glBindVertexArray(vao[i + 10]);
-				glDrawArrays(GL_LINE_LOOP, 0, 4);
-			}
-			if (i == 1) { // 롤러코스터인 경우
-				// 정점 찍기
-				glPointSize(10.0f);
-				glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(border));
-				glUniform3f(objColorLocation, 1.0, 0.0, 0.0);
-				glBegin(GL_POINTS);
-				for (int i = 0; i < 9; ++i)
-					glVertex3f(point[i][0], point[i][1], point[i][2]);
-				glEnd();
+					glBegin(GL_POINTS);
+					for (int i = 0; i < 9; ++i)
+						glVertex3f(point[i][0], point[i][1], point[i][2]);
+					glEnd();
 
-				// 곡선 그리기
-				glLineWidth(7.0f);
-				GLfloat result[4][3];
-				GLfloat t = 0.0f;
-				GLfloat point_x, point_y, point_z;
+					// 곡선 그리기
+					glLineWidth(20.0f);
+					GLfloat result[4][3];
+					GLfloat t = 0.0f;
+					GLfloat point_x, point_y, point_z;
 
-				memset(result, 0, sizeof(result));
-				for (int i = 0; i < 3; ++i) {
-					for (int j = 0; j < 3; ++j) {
-						result[i][0] += matrix_2[i][j] * point[j][0];
-						result[i][1] += matrix_2[i][j] * point[j][1];
-						result[i][2] += matrix_2[i][j] * point[j][2];
-					}
-				}
-				glUniform3f(objColorLocation, 0.0, 1.0, 0.0);
-				glBegin(GL_LINE_STRIP);
-				while (t < 0.5f) {
-					point_x = result[2][0] + t * (result[1][0] + result[0][0] * t);
-					point_y = result[2][1] + t * (result[1][1] + result[0][1] * t);
-					point_z = result[2][2] + t * (result[1][2] + result[0][2] * t);
-					glVertex3f(point_x, point_y, point_z);
-					t += 0.01f;
-				}
-				glEnd();
-
-				for (int cubic_case = 0; cubic_case < 9 - 2; ++cubic_case)
-				{
 					memset(result, 0, sizeof(result));
-					for (int i = 0; i < 4; ++i)
-					{
-						for (int j = 0; j < 4; ++j)
-						{
-							result[i][0] += matrix_3[i][j] * point[j + cubic_case][0];
-							result[i][1] += matrix_3[i][j] * point[j + cubic_case][1];
-							result[i][2] += matrix_3[i][j] * point[j + cubic_case][2];
+					for (int i = 0; i < 3; ++i) {
+						for (int j = 0; j < 3; ++j) {
+							result[i][0] += matrix_2[i][j] * point[j][0];
+							result[i][1] += matrix_2[i][j] * point[j][1];
+							result[i][2] += matrix_2[i][j] * point[j][2];
 						}
 					}
-
-					t = 0.0f;
+					glUniform3f(objColorLocation, 0.0, 1.0, 0.0);
 					glBegin(GL_LINE_STRIP);
-					while (t < 1.0f) {
-						point_x = (result[3][0] + t * (result[2][0] + t * (result[1][0] + result[0][0] * t))) * 0.5f;
-						point_y = (result[3][1] + t * (result[2][1] + t * (result[1][1] + result[0][1] * t))) * 0.5f;
-						point_z = (result[3][2] + t * (result[2][2] + t * (result[1][2] + result[0][2] * t))) * 0.5f;
+					while (t < 0.5f) {
+						point_x = result[2][0] + t * (result[1][0] + result[0][0] * t);
+						point_y = result[2][1] + t * (result[1][1] + result[0][1] * t);
+						point_z = result[2][2] + t * (result[1][2] + result[0][2] * t);
 						glVertex3f(point_x, point_y, point_z);
 						t += 0.01f;
 					}
 					glEnd();
+
+					for (int cubic_case = 0; cubic_case < 9 - 2; ++cubic_case) {
+						memset(result, 0, sizeof(result));
+						for (int i = 0; i < 4; ++i) {
+							for (int j = 0; j < 4; ++j) {
+								result[i][0] += matrix_3[i][j] * point[j + cubic_case][0];
+								result[i][1] += matrix_3[i][j] * point[j + cubic_case][1];
+								result[i][2] += matrix_3[i][j] * point[j + cubic_case][2];
+							}
+						}
+
+						t = 0.0f;
+						glBegin(GL_LINE_STRIP);
+						while (t < 1.0f) {
+							point_x = (result[3][0] + t * (result[2][0] + t * (result[1][0] + result[0][0] * t))) * 0.5f;
+							point_y = (result[3][1] + t * (result[2][1] + t * (result[1][1] + result[0][1] * t))) * 0.5f;
+							point_z = (result[3][2] + t * (result[2][2] + t * (result[1][2] + result[0][2] * t))) * 0.5f;
+							glVertex3f(point_x, point_y, point_z);
+							t += 0.01f;
+						}
+						glEnd();
+					}
+
+					// 원기둥
+					for (int i = 0; i < 8; ++i) {
+						glUniform3f(objColorLocation, 0.3, 0.3, 0.3);
+						glm::mat4 pillar = glm::mat4(1.0f);
+						glm::mat4 pillar_T = glm::mat4(1.0f);
+						glm::mat4 pillar_S = glm::mat4(1.0f);
+						pillar_T = glm::translate(pillar_T, glm::vec3(point[i][0], point[i][1] / 2, point[i][2]));
+						pillar_S = glm::scale(pillar_S, glm::vec3(0.3, point[i][1] / 2, 0.3));
+						pillar = border_T * pillar_T * pillar_S;
+						glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(pillar));
+						glBindVertexArray(vao[1]);
+						glDrawArrays(GL_TRIANGLES, 0, 84);
+					}
+				}
+				else if (i == 2) {   // 관람차인 경우
+
+				}
+				else if (i == 3) {   // 자이로드롭인 경우
+
+				}
+				else if (i == 4) {   // 회전목마인 경우
+
 				}
 			}
-			else if (i == 2) {   // 관람차인 경우
-				 
-			}
-			else if (i == 3) {   // 자이로드롭인 경우
+		}
 
-			}
-			else if (i == 4) {   // 회전목마인 경우
+		// 바닥
+		glm::mat4 floor = glm::mat4(1.0f);
+		glUniform3f(objColorLocation, 0.5, 0.5, 0.5);
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(floor));
+		glBindVertexArray(vao[10]);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
-			}
+		// 나무
+		for (int i = 0; i < tree_cnt; ++i) {
+			glm::mat4 tree = glm::mat4(1.0f);
+			glm::mat4 tree_T = glm::mat4(1.0f);
+			glm::mat4 tree_S = glm::mat4(1.0f);
+			tree_T = glm::translate(tree_T, glm::vec3(tree_x[i], 0.7, tree_z[i]));
+			tree_S = glm::scale(tree_S, glm::vec3(0.3, 0.7, 0.3));
+			tree = tree_T * tree_S;
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(tree));
+			glBindVertexArray(vao[0]);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+
+			glm::mat4 tree_leaf = glm::mat4(1.0f);
+			glm::mat4 tree_leaf_T = glm::mat4(1.0f);
+			glm::mat4 tree_leaf_S = glm::mat4(1.0f);
+			tree_leaf_T = glm::translate(tree_leaf_T, glm::vec3(tree_x[i], 2.5, tree_z[i]));
+			tree_leaf_S = glm::scale(tree_leaf_S, glm::vec3(1.0, 1.5, 1.0));
+			tree_leaf = tree_leaf_T * tree_leaf_S;
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(tree_leaf));
+			glBindVertexArray(vao[2]);
+			glDrawArrays(GL_TRIANGLES, 0, 242);
 		}
 	}
+	else {
+		// top view
+		if (!key_t) {
 
-	// 바닥
-	glm::mat4 floor = glm::mat4(1.0f);
-	glUniform3f(objColorLocation, 0.5, 0.5, 0.5);
-	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(floor));
-	glBindVertexArray(vao[10]);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+		}
+		// front view
+		else {
 
-	// 나무
-	for (int i = 0; i < tree_cnt; ++i) {
-		glm::mat4 tree = glm::mat4(1.0f);
-		glm::mat4 tree_T = glm::mat4(1.0f);
-		glm::mat4 tree_S = glm::mat4(1.0f);
-		tree_T = glm::translate(tree_T, glm::vec3(tree_x[i], 0.7, tree_z[i]));
-		tree_S = glm::scale(tree_S, glm::vec3(0.3, 0.7, 0.3));
-		tree = tree_T * tree_S;
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(tree));
-		glBindVertexArray(vao[0]);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		glm::mat4 tree_leaf = glm::mat4(1.0f);
-		glm::mat4 tree_leaf_T = glm::mat4(1.0f);
-		glm::mat4 tree_leaf_S = glm::mat4(1.0f);
-		tree_leaf_T = glm::translate(tree_leaf_T, glm::vec3(tree_x[i], 2.5, tree_z[i]));
-		tree_leaf_S = glm::scale(tree_leaf_S, glm::vec3(1.0, 1.5, 1.0));
-		tree_leaf = tree_leaf_T * tree_leaf_S;
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(tree_leaf));
-		glBindVertexArray(vao[2]);
-		glDrawArrays(GL_TRIANGLES, 0, 242);
+		}
 	}
 
 	glutSwapBuffers();
@@ -423,6 +471,18 @@ void timer(int value) {
 		cameraPos_x += glm::normalize(glm::cross(cameraDirection, cameraUp)).x;
 		cameraPos_y += glm::normalize(glm::cross(cameraDirection, cameraUp)).y;
 		cameraPos_z += glm::normalize(glm::cross(cameraDirection, cameraUp)).z;
+	}
+	if (rides_install_check[1] == true) {
+
+	}
+	if (rides_install_check[2] == true) {
+
+	}
+	if (rides_install_check[3] == true) {
+
+	}
+	if (rides_install_check[4] == true) {
+
 	}
 	drawScene();
 	glutTimerFunc(100, timer, 1);
@@ -514,6 +574,10 @@ GLvoid Keyboard(unsigned char key, int x, int y) {
 			}
 		}
 		break;
+	case 'c':
+		if(rides_install_check[1] == true)
+			key_c = !key_c;
+		break;
 	case '`':   // 리셋
 		reset();
 		InitBuffer();
@@ -552,10 +616,11 @@ float prev_mouse_x, prev_mouse_y;
 void Mouse(int button, int state, int x, int y) {
 	float normalized_x, normalized_y;
 
-	normalized_x = (2.0 * x / 600) - 1.0;
-	normalized_y = 1.0 - (2.0 * y / 600);
-	if (view_check == 0) {}
+	if (view_check == 0) {
+	}
 	else if (view_check == 1) {
+		normalized_x = (2.0 * x / 600) - 1.0;
+		normalized_y = 1.0 - (2.0 * y / 600);
 		if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
 			left_button = true;
 			prev_mouse_x = normalized_x;  // 저장된 이전 마우스 위치
@@ -565,19 +630,47 @@ void Mouse(int button, int state, int x, int y) {
 			left_button = false;
 		}
 	}
+	if (key_c) {
+		normalized_x = ((2.0 * x / 600) - 1.0) * 15;
+		normalized_y = ((2.0 * y / 600) - 1.0) * 15;
+		if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && rides_install_check[1] == true && rides_sel_cnt == 1) {
+			for (int i = 0; i < 8; ++i) {
+				if (normalized_x > point[i][0] - 1.0 && normalized_x < point[i][0] + 1.0 &&
+					normalized_y > point[i][2] - 1.0 && normalized_y < point[i][2] + 1.0 ) {
+					point_sel[i] = true;
+					point_sel_index = i;
+					break;
+				}
+			}
+		}
+	}
 }
 
 void Motion(int x, int y) {
 	float normalized_x, normalized_y;
 
 	if (view_check == 0) {
+		normalized_x = ((2.0 * x / 600) - 1.0) * 15;
+		normalized_y = ((2.0 * y / 600) - 1.0) * 15;
 		if (rides_install_check[rides_sel_cnt] == false) {
-			normalized_x = ((2.0 * x / 600) - 1.0) * 15;
-			normalized_y = ((2.0 * y / 600) - 1.0) * 15;
+
 			if (rides_sel_check[rides_sel_cnt] == true) {
 				rides_x[rides_sel_cnt] = normalized_x;
 				rides_z[rides_sel_cnt] = normalized_y;
 				rides_collision_check();
+			}
+		}
+		if (point_sel[point_sel_index] == true) {
+			if (normalized_x >= rides_x[1] - 8 && normalized_x <= rides_x[1] + 8 &&
+				normalized_y >= rides_z[1] - 2.5 && normalized_y <= rides_z[1] + 2.5) {
+				if (point_sel_index == 0) {
+					point[0][0] = point[8][0] = normalized_x;
+					point[0][2] = point[8][2] = normalized_y;
+				}
+				else {
+					point[point_sel_index][0] = normalized_x;
+					point[point_sel_index][2] = normalized_y;
+				}
 			}
 		}
 	}
